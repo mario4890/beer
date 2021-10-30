8347e25ca9f1616508401b771186a83ed34dbacf
mario4890 <poczta@mariuszb.eu>
2021-10-30 13:43:36 +0200

    configure webpack and add some style

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card__container">
+            <form action="{{ route('makeTemperature') }}" method="post">
+                @csrf
+                <div class="table">
+                    <div class="table__container">
+                        <label class="table__row">
+                            <div class="table__col--left">
+                                <span>City:</span>
+                            </div>
+                            <div class="table__col--right">
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@endsection
